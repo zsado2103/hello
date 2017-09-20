f813c05493dd270f5004eef3413cd802853531a3
zsado2103 <zanetasado@gmail.com>
2017-09-20 12:35:46 +0200

    pobieranie danych tekstowych

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -5,10 +5,19 @@
 
 
 #include <iostream>
+
 using namespace std;
+
 int main(int argc, char **argv)
 {
-	cout << "Witaj w C++!";
+    //char imie; // deklaracja zmiennej znakowej
+    char imie[10]; //deklaracja tablicy znakowej
+
+	cout << "Witaj w C++!" << endl;
+	cout << "Podaj imię";
+    //cin >> imie;
+    cin.getline(imie, 10) ;
+    cout << "Cześć " << imie << endl;
 	return 0;
 }
 
