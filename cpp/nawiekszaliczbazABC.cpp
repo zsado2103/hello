9cd4fdd0ffcb03a320e6f60489d2c53df19e60d9
zsado2103 <zanetasado@gmail.com>
2017-09-27 11:56:02 +0200

    najwiekszaliczbazABC

---
+++ cpp/nawiekszaliczbazABC.cpp
@@ -0,0 +1,59 @@
+/*
+* najwieksza.cpp
+* Pobierz trzy liczby całkowite od użytkownika i wydrukuj największą
+*/
+
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char **argv)
+
+{
+    int a, b, c;
+    a = b = c = 0;
+    cout << "Podaj 3 liczby: ";
+    cin >> a >> b >> c;
+    
+    if (a > b) 
+    {
+        if (a > c) 
+            cout << "Największe a=" << a;
+        else 
+            cout << "Największe c=" << c;
+    } 
+    else if (b > a) 
+    {
+        if (b > c) 
+        cout << "Największe b=" << b;
+        else
+            cout << "Największe c=" << c;
+    } 
+        
+    if (a > b) 
+    {
+        if (a > c) 
+            cout << "Największe a=" << a;
+        else 
+            cout << "Największe c=" << c;
+    } 
+    else if (b > a) 
+    {
+        if (b > c) 
+        cout << "Największe b=" << b;
+        else
+            cout << "Największe c=" << c;
+    } 
+
+    if (a==b)
+        {
+            if ( a > c )
+                cout << "Największe a=" << a;
+                else if (b == c)
+                cout << "Liczby są równe a = b = c =" << a;
+                else if (a < c)
+                cout << "Największe c=" << c;
+        }
+
+    return 0;
+}
