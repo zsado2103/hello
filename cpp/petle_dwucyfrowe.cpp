193af1214254354b0e22ae5afc41538897baf20a
zsado2103 <zanetasado@gmail.com>
2017-10-17 20:30:31 +0200

    petla_dwucyfrowa

--- cpp/petle_dwucyfrowe.cpp
+++ cpp/petle_dwucyfrowe.cpp
@@ -10,15 +10,40 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	int m, n;
-    cout <<"Podaj przedział <m, n> <10,99> :  " endl;
-    cin >> m >> n;
-        while (true) {
-            if () break;
-            
-            cout <<"Podaj przedział <m, n> <10,99> :  " endl;
-            cin >> m >> n;
-            }
-    
+   
+       cout <<"Podaj przedział <m, n> <10,99> :  " << endl;
+	   cin >> m >> n;
+			
+            while (m > n || n < 10 || n > 99 || m <10 || m > 99)
+            {
+			cout <<"Podaj przedział <m, n> <10,99> :  " << endl;
+			 cin >> m >> n;
+			 }
+            while (m <= n)
+            {
+             
+			if (m % 3 == 0)
+			{
+			cout << m << endl;
+			}
+			else if ((m+1)% 3 == 0)
+			{
+			cout << m+1 << endl;
+			}
+			else if ((m+2) % 3 == 0)
+			{
+			cout << m+2 << endl;
+			}
+			
+			m += 3;
+		}
+			
+		 
+
+			
+		 
+
+   
 
 	return 0;
 }
