4995762c9f0a68c73bdbb6f258711f1da5b0e667
zsado2103 <zanetasado@gmail.com>
2017-10-31 13:31:22 +0100

    python

---
+++ Python/witaj.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  witaj.py
+
+# osoba = "Adam Słodowy"
+# osoba = 'Adam Słodowy'
+osoba = input('Jak się nazywasz?\n ') #\n- następny wiersz
+print("Witaj!",osoba, "")
+
+rok = 2017
+wiek = int(input('Ile masz lat?\n'))
+print('Urodziłeś się w ', rok - wiek, 'roku!')
