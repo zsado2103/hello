72a879bb85506a637108e7552c32600131413d6f
zsado2103 <zanetasado@gmail.com>
2017-11-08 12:36:05 +0100

    mlotek

--- Python/mlotek.py
+++ Python/mlotek.py
@@ -4,16 +4,29 @@
 from random import randint
 
 
+def losuj(ileliczb, maksliczb):
+    liczby = []
+    ile = 0  # ilość wylosowanych liczb
+    while ile < ileliczb:
+        # for i in range(ileliczb):
+        liczba = randint(0, maksliczb)
+        if liczby.count(liczba) == 0:
+            liczby.append(liczba)
+            ile += 1
+    print(liczby)
+
+
 def main(args):
     ileliczb = int(input("Ile liczb chcesz zgadnąć? "))
     maksliczb = int(input("Podaj górny zakres: "))
 
-    liczby = []
+    losuj(ileliczb, maksliczb)
+    typy = set()  # pusty zbiór
     for i in range(ileliczb):
-        liczba = randint(0, maksliczb)
-        if liczby.count(liczba) == 0:
-            liczby.append(liczba)
-    print(liczby)
+        typ = input("Podaj typ: ")
+        typy.add(typ)
+
+    print(typy)
 
     return 0
 
