f308b82014be5b7e724e9ebd05af87795c9c8c11
zsado2103 <zanetasado@gmail.com>
2017-11-17 11:41:46 +0100

    silnia

--- Python/potega.py
+++ Python/potega.py
@@ -9,13 +9,23 @@ def potega_it(podst, wykladnik):
     """Funkcja oblicza  iteracyjnie potęgę liczby naturalnej"""
     wynik = 1
     for i in range(wykladnik):
-        wynik = wynik*i
+        wynik = wynik * podst
     return wynik
 
 
 def main(args):
     #  pobierz od uzytkownika podstawę i wykładnik i przypisz do odpowiednich #  zmiennych
     #  wywolaj funkcje potega_it()
+    a = int(input("Podaj podstawę: "))
+    n = int(input("Podaj wykładnik: "))
+    assert type(a) == int
+    assert type(n) == int
+
+    assert potega_it(100, 0) == 1
+    assert potega_it(100, 1) == 100
+    assert potega_it(2, 3) == 8
+
+    # print("Potęga: ", potega_it(a, n))
 
     return 0
 
---
+++ Python/silnia.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  n! = 1 dla {0,1}
+#  n! = 1 * 2 * ... * n dla N+ - {0, 1}
+
+
+def silnia_it(n):
+    """Funkcja oblicza  iteracyjnie silnię  liczby naturalnej"""
+    wynik = 1
+    for i in range(2, n + 1):
+        wynik = wynik * i
+    return wynik
+
+
+def main(args):
+    a = int(input("Podaj liczbę: "))
+    assert type(a) == int
+
+    assert silnia_it(0) == 1
+    assert silnia_it(1) == 1
+    assert silnia_it(7) == 5040
+    # print("Potęga: ", potega_it(a, n))
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
