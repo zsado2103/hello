d9d0f33da4fc6b5196cf94a8f4df68e8e3fa849d
zsado2103 <zanetasado@gmail.com>
2017-11-30 22:43:19 +0100

    Fibonacci

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -6,23 +6,30 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-
-int fib_iter(int n){
-    int a = 0
-    int b = 0
-    if (n == 0) {
-        cout >> 0;
-        return 0;
-    
-    }
-    cout << a;
+int fib_iter(int n)
+{
+    int a = 0;
+    int b = 1;
+	if (n == 0)
+	{	
+		cout << 0;
+		return 0;
+	}
+	cout << a;
     for(int i=2; i < n; i++)
-    }
-    
-    
+    {
+	cout << b << " ";
+		b += a;
+        a = b-a;
+	}
+}
+
 int main(int argc, char **argv)
 {
-	
+	int n;
+	cout << "Podaj liczbe " << endl;
+	cin >> n;
+	fib_iter(n);
 	return 0;
 }
 
