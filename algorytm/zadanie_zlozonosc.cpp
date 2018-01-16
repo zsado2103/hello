fbb52b4d478c84a8db11872a120dfd511d9ea2bc
zsado2103 <zanetasado@gmail.com>
2018-01-16 13:30:44 +0100

    alg

--- algorytm/zadanie_zlozonosc.cpp
+++ algorytm/zadanie_zlozonosc.cpp
@@ -8,14 +8,19 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	int n = 0;
+    
     cout << "Podaj n: " << endl;
     cin >> n;
     
     int i = 1;
+    
     while (i < n){
         i += 2;
-        cout << i << endl;
-    }
+        cout << i << endl; }
+    // for(int i = 1; i <= n; i +=2){
+    // cout << i << " ";
+       
+    
 	
 	return 0;
 }
