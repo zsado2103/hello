960b6694a215e257ff328945ab1759a9aacfb34b
zsado2103 <zanetasado@gmail.com>
2018-02-06 21:42:17 +0100

    szyfr

--- Python/szyfr_Cezara.py
+++ Python/szyfr_Cezara.py
@@ -7,19 +7,35 @@ def szyfruj(tekst, klucz):
     szyfrogram = ""
     klucz = klucz % 26
     for znak in tekst:
-        ascii = ord(znak) + klucz
+        if ord(znak)>64 and ord(znak)<91:
+            ascii = ord(znak) + klucz
         if ascii > 90:
             ascii -= 26
+        if ord(znak)>96 and ord(znak)<123:
+            ascii = ord(znak) + klucz
         szyfrogram += chr(ascii)
     return szyfrogram
 
+
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    klucz = klucz % 26
+    for znak in szyfrogram:
+        if ord(znak) > 64 and ord(znak) < 91:
+            ascii = ord(znak) - klucz
+            if ascii > 90:
+                ascii -= 26
+        if ord(znak) > 96 and ord(znak) < 123:
+            ascii = ord(znak) - klucz
+        tekst += chr(ascii)
+    return tekst
 
 def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Podaj klucz: "))
     szyfrogram = szyfruj(tekst, klucz)
     print(szyfrogram)
-
+    print(deszyfruj(szyfrogram, klucz))
     return 0
 
 if __name__ == '__main__':
