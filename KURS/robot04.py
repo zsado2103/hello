ab52d5bc81529d9bbe020c00695192ad9406388c
zsado2103 <zanetasado@gmail.com>
2019-02-05 15:59:23 +0100

    aksd

--- KURS/robot04.py
+++ KURS/robot04.py
@@ -6,6 +6,10 @@ import rg
 # 192.168.3.10:8000
 class Robot:
 
+    def czy_wejscie(self,poz):
+        if 'spawn' rg.loc_types(poz)
+        return, True
+
     def znajdz_wrogow_obok(self, game):
         wrogowie = []
         for poz in rg.locs_around(self.location, filter_out=('invalid', 'obstacle')):
@@ -15,14 +19,18 @@ class Robot:
         return wrogowie
 
     def act(self, game):
+        dzialanie = ['guard']
+        
         wrogowie = self.znajdz_wrogow_obok(game)
         print(wrogowie)
 
-        if len(wrogowie) == 1 and self.hp > 10:
-            return['attack', wrogowie[0]]
-
         if self.location != rg.CENTER_POINT:
-            return['move', rg.toward(self.location, rg.CENTER_POINT)]
+            dzialanie = ['move', rg.toward(self.location, rg.CENTER_POINT)]
+
+        if len(wrogowie) == 1 and self.hp > 10:
+            dzialanie = ['attack', wrogowie[0]]
         
-        return['guard']
+        return dzialanie
+        
+
         
